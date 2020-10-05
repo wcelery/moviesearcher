3e53b79f2242a7710b79f225f44ca01a1b7f3351
wcelery <horoschun.yura@yandex.ru>
2020-10-05 16:04:29 +0800

    similars implemented

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import Similar from "./Similar";
+import { useLocation } from "react-router-dom";
 
 export default function MovieDetails(props) {
   const [details, setDetails] = React.useState({});
   const [genres, setGenres] = React.useState([]);
   const [similars, setSimilars] = React.useState([]);
+
+  let location = useLocation();
 
   const API_KEY = "3898867ebc97917c67c0d9841df34dce";
 
@@ -14,33 +17,21 @@ export default function MovieDetails(props) {
       `https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=${API_KEY}`
     );
     const details = await data.json();
+    const genres = await details.genres.map((genre) => genre.name);
+    const genresId = await details.genres.map((genre) => genre.id).toString();
     setDetails(details);
-  };
-
-  const fetchSimilar = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=28&include_adult=false&include_video=false&page=1`
+    setGenres(genres);
+    const test = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=${genresId}&include_adult=false&include_video=false&page=1`
     );
-    const similar = await data.json();
-    similar.results.length = 4;
+    const similar = await test.json();
+    similar.results.length = 5;
     setSimilars(similar.results);
   };
 
   React.useEffect(() => {
     fetchMovieDetail();
-    fetchSimilar();
-  }, []);
-
-  React.useEffect(() => {
-    fetchGenres();
-  }, [details]);
-
-  const fetchGenres = () => {
-    if (details.genres) {
-      const test = details.genres.map((genre) => genre.name);
-      setGenres(test);
-    }
-  };
+  }, [location]); //rerender component if URL was changed
 
   return (
     <div className="wrapper">
@@ -92,6 +83,7 @@ export default function MovieDetails(props) {
             key={uuidv4()}
             poster={similar.poster_path}
             title={similar.title}
+            id={similar.id}
           />
         ))}
       </div>
--- src/components/Similar.js
+++ src/components/Similar.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Similar({ poster, title }) {
+export default function Similar({ poster, id }) {
   return (
     <div>
-      <img
-        src={`https://image.tmdb.org/t/p/original${poster}`}
-        alt=""
-        width="210"
-        className="img-similar"
-      />
+      <Link to={`/${id}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/original${poster}`}
+          alt=""
+          width="210"
+          className="img-similar"
+        />
+      </Link>
     </div>
   );
 }
