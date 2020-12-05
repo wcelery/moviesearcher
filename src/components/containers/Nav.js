821d23b98c7bebd21e8e446f730011667d5a5aab
wcelery <horoschun.yura@yandex.ru>
2020-12-05 15:46:09 +0800

    some changes

--- src/components/containers/Nav.js
+++ src/components/containers/Nav.js
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <div className="nav">
-      <h3>logo</h3>
+      <h3>mvsrchr</h3>
       <div className="nav-links">
         <Link to="/favorites">
-          <li>some link</li>
+          <li>favorites</li>
         </Link>
-        <Link to="/favorites">
-          <li>another link</li>
-        </Link>
+        <li>another link</li>
       </div>
     </div>
   );
