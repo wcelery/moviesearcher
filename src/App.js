4f362b5ddc97ba25d78469bfe5312e35081c579e
wcelery <horoschun.yura@yandex.ru>
2021-03-19 15:12:03 +0800

    migrate to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Nav from "./components/containers/Nav";
